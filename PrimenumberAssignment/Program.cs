5ec9af70d0c91c171e7da0be19d4bd8a69c9fe47
Marcus Sjöholm <marcusasjoholm93@gmail.com>
2021-08-31 17:35:10 +0200

    Add project files.

---
+++ PrimenumberAssignment/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PrimenumberAssignment
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Entry point of program
+        /// </summary>
+        public static void Main()
+        {
+            PrimeProgram.MenuLoop();
+        }
+    }
+}
---
+++ PrimenumberAssignment/ProgramBase.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimenumberAssignment
+{
+    internal static class PrimeProgram
+    {
+        /// <summary>
+        /// List of all saved prime numbers
+        /// </summary>
+        public static List<int> savedPrimeNumbers = new List<int>();
+
+        /// <summary>
+        /// Asks user to input an integer, then sending the number to be evaluated
+        /// </summary>
+        public static void AddPotentialPrimeNumber()
+        {
+            Console.WriteLine("Enter a number: ");
+            var num = Convert.ToInt32(Console.ReadLine());
+            bool isPrime = PrimeChecker(num);
+            IsNumberPrime(num, isPrime);
+        }
+
+        public static void ClosingMessage()
+        {
+            Console.WriteLine("Program is closing");
+        }
+
+        /// <summary>
+        /// Display saved prime numbers in console
+        /// Will give an error if the list is empty
+        /// </summary>
+        /// <returns>Goes through and print prime numbers</returns>
+        public static List<int>.Enumerator DisplaySavedPrimeNumbers()
+        {
+            if (savedPrimeNumbers.Count == 0)
+            {
+                ListIsEmpty();
+            }
+
+            var en = savedPrimeNumbers.GetEnumerator();
+
+            while (en.MoveNext())
+            {
+                Console.WriteLine($"{en.Current}");
+            }
+
+            return en;
+        }
+
+        /// <summary>
+        /// Show exception error from try-catch
+        /// </summary>
+        /// <param name="ex"></param>
+        public static void ExceptionError(Exception ex)
+        {
+            Console.WriteLine("Error: " + ex);
+        }
+
+        /// <summary>
+        /// After checking if users input is prime or not, it will either save the number to the
+        /// list or tell user it´s not a prime and enter another number
+        /// </summary>
+        /// <param name="num">Number input from user</param>
+        /// <param name="isPrime">Is the number prime? True or False</param>
+        public static void IsNumberPrime(int num, bool isPrime)
+        {
+            try
+            {
+                if (isPrime)
+                {
+                    if (num == 1)
+                    {
+                        Console.WriteLine($"{num} is not a prime number");
+                        NewLineInConsole();
+                    }
+                    else
+                    {
+                        Console.WriteLine(num + " is a prime number");
+                        SavePrimenumbers(num);
+                        NewLineInConsole();
+                    }
+                }
+                else if (!isPrime)
+                {
+                    Console.WriteLine($"{num} is not a prime number");
+                    NewLineInConsole();
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionError(ex);
+            }
+        }
+
+        /// <summary>
+        /// Displays an error if list is empty
+        /// </summary>
+        public static void ListIsEmpty()
+        {
+            Console.WriteLine("The list is empty");
+        }
+
+        /// <summary>
+        /// Checks if user input a number, will give an error message if user write anything else
+        /// than what menu offers
+        /// </summary>
+        public static void LoopMenuOptions()
+        {
+            var userInput = MenuOptions();
+            try
+            {
+                if (!int.TryParse(userInput, out _))
+                {
+                    WrongInput();
+                }
+                else
+                {
+                    RunProgramFromUserChoice(userInput);
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionError(ex);
+            }
+        }
+
+        /// <summary>
+        /// Loops the menu
+        /// </summary>
+        public static void MenuLoop()
+        {
+            while (true)
+            {
+                LoopMenuOptions();
+            }
+        }
+
+        /// <summary>
+        /// Shows menu options and let user input their choice
+        /// </summary>
+        /// <returns>Returns what user choose from menu</returns>
+        public static string MenuOptions()
+        {
+            MenuOptionText();
+            string userInput = Console.ReadLine();
+            Console.Clear();
+            return userInput;
+        }
+
+        /// <summary>
+        /// The different options the user have to choose from
+        /// </summary>
+        public static void MenuOptionText()
+        {
+            Console.WriteLine("Enter '1' to check if a number is a prime number " +
+                            "\nEnter '2' to see stored prime numbers " +
+                            "\nEnter '3' Show next prime number " +
+                            "\nEnter '4' to exit program");
+        }
+
+        /// <summary>
+        /// Adds a new line in console to make UI easier to read
+        /// </summary>
+        public static void NewLineInConsole()
+        {
+            Console.WriteLine("\n");
+        }
+
+        /// <summary>
+        /// Adds one to highest known prime number and when the program finds next primenumber it
+        /// stops and adds it to the list of saved prime numbers
+        /// </summary>
+        public static void NextPrimeNumber()
+        {
+            var highest = savedPrimeNumbers[^1] + 1;
+
+            while (true)
+            {
+                try
+                {
+                    if (PrimeChecker(highest))
+                    {
+                        SavePrimenumbers(highest);
+                        return;
+                    }
+                    else
+                    {
+                        highest++;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ExceptionError(ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks if the input number is a prime or not
+        /// </summary>
+        /// <param name="num">Number input from user</param>
+        /// <returns></returns>
+        public static bool PrimeChecker(int num)
+        {
+            bool isPrime = true;
+            try
+            {
+                for (var p = 2; p < num; p++)
+                {
+                    if (num % p == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionError(ex);
+            }
+
+            return isPrime;
+        }
+
+        /// <summary>
+        /// Checks users input and execute it
+        /// </summary>
+        /// <param name="userInput">Users input from menu</param>
+        public static void RunProgramFromUserChoice(string userInput)
+        {
+            try
+            {
+                switch (userInput)
+                {
+                    case "1":
+                        {
+                            AddPotentialPrimeNumber();
+                            break;
+                        }
+
+                    case "2":
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Saved prime numbers:");
+                            DisplaySavedPrimeNumbers();
+                            NewLineInConsole();
+                            break;
+                        }
+
+                    case "3":
+                        {
+                            Console.WriteLine("Next prime is displayed incrementally");
+                            NextPrimeNumber();
+                            DisplaySavedPrimeNumbers();
+                            NewLineInConsole();
+                            break;
+                        }
+
+                    case "4":
+                        {
+                            ClosingMessage();
+                            Environment.Exit(1);
+                            break;
+                        }
+
+                    default:
+                        {
+                            WrongInput();
+                            break;
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionError(ex);
+            }
+        }
+
+        /// <summary>
+        /// Saves prime numbers to list, if the number already exsists it will not be added again to
+        /// remove redundancy, also sorts the prime numbers incrementally
+        /// </summary>
+        /// <param name="num">Number input from user</param>
+        public static void SavePrimenumbers(int num)
+        {
+            try
+            {
+                if (!savedPrimeNumbers.Contains(num))
+                {
+                    savedPrimeNumbers.Add(num);
+                    savedPrimeNumbers.Sort();
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionError(ex);
+            }
+        }
+
+        /// <summary>
+        /// Display in console that the user
+        /// didn´t choose any of the available choices
+        /// </summary>
+        public static void WrongInput()
+        {
+            Console.WriteLine("Wrong input, please choose a number between 1-4");
+            NewLineInConsole();
+        }
+    }
+}
